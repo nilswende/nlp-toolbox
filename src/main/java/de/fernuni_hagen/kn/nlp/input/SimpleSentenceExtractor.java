bc658baba71414c86099a810ec5e328ec319b499
nilswende <s142075@student.dhbw-mannheim.de>
2020-12-10 11:48:09 +0100

    Extract sentence file corpus #3

--- src/main/java/de/fernuni_hagen/kn/nlp/input/SimpleSentenceExtractor.java
+++ src/main/java/de/fernuni_hagen/kn/nlp/input/SimpleSentenceExtractor.java
@@ -12,7 +12,9 @@ import java.nio.CharBuffer;
 import java.text.BreakIterator;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Stream;
 
 /**
@@ -43,6 +45,22 @@ public class SimpleSentenceExtractor implements SentenceExtractor {
 		}
 	}
 
+	private BreakIterator getBreakIterator(final File textFile) {
+		final Locale locale = getLocale(textFile);
+		return BreakIterator.getSentenceInstance(locale);
+	}
+
+	private Locale getLocale(final File textFile) {
+		final var availableLocales = Arrays.asList(BreakIterator.getAvailableLocales());
+		final var language = languageExtractor.extract(textFile);
+		final var priorityList = Collections.singletonList(new Locale.LanguageRange(language.toLanguageTag()));
+		final var bestMatch = Locale.lookup(priorityList, availableLocales);
+		if (bestMatch == null) {
+			throw new IllegalArgumentException("BreakIterator does not support language " + language);
+		}
+		return bestMatch;
+	}
+
 	private List<String> extractSentences(final File textFile, final BreakIterator boundary) throws IOException {
 		try (final var fileReader = new BufferedFileReader(textFile, Config.DEFAULT_CHARSET)) {
 			final var sentences = new ArrayList<String>();
@@ -52,17 +70,8 @@ public class SimpleSentenceExtractor implements SentenceExtractor {
 				 start = end, end = boundary.next()) {
 				sentences.add(extractSentence(fileReader, start, end));
 			}
-			return sentences; //TODO lazily populate stream?
+			return sentences;
 		}
-	}
-
-	private BreakIterator getBreakIterator(final File textFile) {
-		final var availableLocales = Arrays.asList(BreakIterator.getAvailableLocales());
-		final var language = languageExtractor.extract(textFile);
-		if (!availableLocales.contains(language)) { //TODO Locale Matching
-			throw new IllegalArgumentException("BreakIterator does not support language " + language);
-		}
-		return BreakIterator.getSentenceInstance(language);
 	}
 
 	private String extractSentence(final BufferedFileReader fileReader, final int start, final int end) {
