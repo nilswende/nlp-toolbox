1bc3fd6de9204f6961bc5949e844aea9c236a374
nilswende <s142075@student.dhbw-mannheim.de>
2021-03-29 13:03:17 +0200

    db nachimpl+tests

--- src/test/java/de/fernuni_hagen/kn/nlp/db/im/DBTestIm.java
+++ src/test/java/de/fernuni_hagen/kn/nlp/db/im/DBTestIm.java
@@ -3,9 +3,14 @@ package de.fernuni_hagen.kn.nlp.db.im;
 import de.fernuni_hagen.kn.nlp.config.AppConfig;
 import de.fernuni_hagen.kn.nlp.db.DBTest;
 import de.fernuni_hagen.kn.nlp.db.factory.DBFactory;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Nils Wende
@@ -13,6 +18,8 @@ import java.nio.file.Path;
 public class DBTestIm extends DBTest {
 
 	private static final DBFactory dbFactory;
+
+	private final InMemoryDB db = (InMemoryDB) dbFactory.getDb();
 
 	static {
 		final var mock = Mockito.mock(AppConfig.class);
@@ -24,5 +31,47 @@ public class DBTestIm extends DBTest {
 	@Override
 	protected DBFactory getDbFactory() {
 		return dbFactory;
+	}
+
+	@Test
+	void addSentence() {
+		final var input = List.of("art", "competition", "game", "year");
+		writer.addSentence(input);
+
+		final var data = db.getData();
+
+		assertEquals(input.size(), data.size());
+		data.forEach((k, v) -> {
+			assertEquals(1, v.getCount());
+
+			final var terms = new HashSet<>(input);
+			terms.remove(k);
+			final var cooccs = v.getCooccs();
+			assertEquals(terms, cooccs.keySet());
+		});
+	}
+
+	@Test
+	void add2Sentences() {
+		final var input = List.of(
+				List.of("art", "competition", "game", "year"),
+				List.of("art", "artist", "amateur")
+		);
+		input.forEach(writer::addSentence);
+
+		final var data = db.getData();
+
+		assertEquals(input.stream().mapToInt(List::size).sum() - 1, data.size());
+
+		final var art = data.get("art");
+		assertEquals(2, art.getCount());
+		assertEquals(5, art.getCooccs().size());
+
+		final var artist = data.get("artist");
+		assertEquals(2, artist.getCooccs().size());
+		final var game = data.get("game");
+		assertEquals(3, game.getCooccs().size());
+
+		data.entrySet().stream().filter(e -> !e.getKey().equals("art")).forEach(e -> assertEquals(1, e.getValue().getCount()));
 	}
 }
