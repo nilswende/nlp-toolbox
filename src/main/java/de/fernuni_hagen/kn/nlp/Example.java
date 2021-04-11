0fa937c094e0d5d4fb55acd3d76eb7d77a2cb624
nilswende <s142075@student.dhbw-mannheim.de>
2021-04-11 16:33:26 +0200

    example

---
+++ src/main/java/de/fernuni_hagen/kn/nlp/Example.java
@@ -0,0 +1,35 @@
+package de.fernuni_hagen.kn.nlp;
+
+import de.fernuni_hagen.kn.nlp.analysis.PageRank;
+import de.fernuni_hagen.kn.nlp.config.AppConfig;
+import de.fernuni_hagen.kn.nlp.preprocessing.Preprocessor;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * An example of how to use the NLPToolbox.
+ *
+ * @author Nils Wende
+ */
+public class Example {
+
+	/**
+	 * An example of how to use the NLPToolbox.
+	 *
+	 * @param args the document text
+	 */
+	public static void main(final String[] args) {
+		final var docText = String.join(StringUtils.SPACE, args);
+		// create the app config
+		final var appConfig = new AppConfig().setBaseDir("").setDbDir("any");
+		// create the use case steps
+		final var preprocessor = new Preprocessor()
+				.setInput(docText)
+				.setDocumentName("any");
+		final var pageRank = new PageRank().setResultLimit(5);
+		// run the NLPToolbox
+		new NLPToolbox(appConfig, preprocessor, pageRank).run();
+		// process the results
+		System.out.println(pageRank.getResult());
+	}
+
+}
