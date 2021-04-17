75b197a44c1db073528ff0c3e933acec2d1fb261
nilswende <s142075@student.dhbw-mannheim.de>
2021-04-17 18:38:14 +0200

    usages

---
+++ src/test/java/usage/Analyze.java
@@ -0,0 +1,37 @@
+package usage;
+
+import de.fernuni_hagen.kn.nlp.NLPToolbox;
+import de.fernuni_hagen.kn.nlp.UseCase;
+import de.fernuni_hagen.kn.nlp.analysis.HITS;
+import de.fernuni_hagen.kn.nlp.analysis.PageRank;
+import de.fernuni_hagen.kn.nlp.config.AppConfig;
+import de.fernuni_hagen.kn.nlp.math.WeightingFunction;
+
+import java.time.Duration;
+import java.util.List;
+
+import static de.fernuni_hagen.kn.nlp.Logger.logCurrentThreadCpuTime;
+
+/**
+ * @author Nils Wende
+ */
+public class Analyze {
+
+	public static void main(final String[] args) {
+		// create the app config
+		final var appConfig = new AppConfig();
+		// create the use case steps
+		final var pageRank = new PageRank().setResultLimit(10).setWeightingFunction(WeightingFunction.ASSN);
+		final var hits = new HITS().setResultLimit(10).setWeightingFunction(WeightingFunction.ASSN);
+		final var useCases = List.of(pageRank, hits);
+		// run the NLPToolbox
+		final var start = System.nanoTime();
+		new NLPToolbox(appConfig, useCases).run();
+		final var duration = Duration.ofNanos(System.nanoTime() - start);
+		// process the results
+		useCases.stream().map(UseCase::getResult).forEach(System.out::println);
+		System.out.println(String.format("total duration: %d s %d ms", duration.toSecondsPart(), duration.toMillisPart()));
+		logCurrentThreadCpuTime();
+	}
+
+}
---
+++ src/test/java/usage/SaveDB.java
@@ -0,0 +1,42 @@
+package usage;
+
+import de.fernuni_hagen.kn.nlp.NLPToolbox;
+import de.fernuni_hagen.kn.nlp.UseCase;
+import de.fernuni_hagen.kn.nlp.config.AppConfig;
+import de.fernuni_hagen.kn.nlp.db.ClearDatabase;
+import de.fernuni_hagen.kn.nlp.preprocessing.FilePreprocessor;
+
+import java.time.Duration;
+import java.util.List;
+
+import static de.fernuni_hagen.kn.nlp.Logger.logCurrentThreadCpuTime;
+
+/**
+ * @author Nils Wende
+ */
+public class SaveDB {
+
+	public static void main(final String[] args) {
+		// create the app config
+		final var appConfig = new AppConfig().setPersistInMemoryDb(true);
+		// create the use case steps
+		final var clearDatabase = new ClearDatabase();
+		final var preprocessor = new FilePreprocessor()
+				.setKeepTempFiles(false)
+				.setExtractPhrases(false)
+				.setUseBaseFormReduction(true)
+				.setFilterNouns(true)
+				.setRemoveStopWords(true)
+				.setNormalizeCase(true);
+		final var useCases = List.of(clearDatabase, preprocessor);
+		// run the NLPToolbox
+		final var start = System.nanoTime();
+		new NLPToolbox(appConfig, useCases).run();
+		final var duration = Duration.ofNanos(System.nanoTime() - start);
+		// process the results
+		useCases.stream().map(UseCase::getResult).forEach(System.out::println);
+		System.out.println(String.format("total duration: %d s %d ms", duration.toSecondsPart(), duration.toMillisPart()));
+		logCurrentThreadCpuTime();
+	}
+
+}
