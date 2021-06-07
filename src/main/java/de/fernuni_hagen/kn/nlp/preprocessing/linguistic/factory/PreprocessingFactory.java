ef78f8c60f37e3245704009c950d47d16810672e
nilswende <s142075@student.dhbw-mannheim.de>
2021-06-07 18:53:38 +0200

    implement caching for resources in ASV #54

--- src/main/java/de/fernuni_hagen/kn/nlp/preprocessing/linguistic/factory/PreprocessingFactory.java
+++ src/main/java/de/fernuni_hagen/kn/nlp/preprocessing/linguistic/factory/PreprocessingFactory.java
@@ -12,6 +12,8 @@ import de.fernuni_hagen.kn.nlp.preprocessing.linguistic.Tagger;
 import de.fernuni_hagen.kn.nlp.preprocessing.linguistic.impl.JLanILanguageExtractor;
 
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Defines a factory for the components used in preprocessing (Abstract Factory Pattern).
@@ -47,13 +49,44 @@ public interface PreprocessingFactory {
 	static PreprocessingFactory from(final Path textFile) {
 		final var languageExtractor = new JLanILanguageExtractor();
 		final var locale = languageExtractor.extract(textFile);
-		switch (locale.getLanguage()) {
+		final var language = locale.getLanguage();
+		return Cache.get(language);
+	}
+
+	/**
+	 * Creates a concrete factory according to the given language.
+	 *
+	 * @param language a language
+	 * @return a concrete factory
+	 */
+	static PreprocessingFactory from(String language) {
+		switch (language) {
 			case "de":
 				return new DEFactory();
 			case "en":
 				return new ENFactory();
 			default:
-				throw new IllegalArgumentException("Unsupported locale: " + locale);
+				throw new IllegalArgumentException("Unsupported language: " + language);
+		}
+	}
+
+	/**
+	 * Caches the created {@link PreprocessingFactory}s.<br>
+	 * This high-level optimization is only possible because the preprocessing is entirely single threaded.
+	 *
+	 * @author Nils Wende
+	 */
+	class Cache {
+		private static final Map<String, PreprocessingFactory> cache = new HashMap<>();
+
+		/**
+		 * Returns a concrete factory according to the given language.
+		 *
+		 * @param language a language
+		 * @return a concrete factory
+		 */
+		static PreprocessingFactory get(final String language) {
+			return cache.computeIfAbsent(language, PreprocessingFactory::from);
 		}
 	}
 
